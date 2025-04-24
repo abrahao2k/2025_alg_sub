d248ea1ccc2466bc6d813d061b3046b5004d66a1
Abrahão Lopes <117474324+abrahao2k@users.noreply.github.com>
2025-04-24 10:25:22 -0300

    Delete questao8.py

--- questao8.py
+++
@@ -1,9 +0,0 @@
-'''8. Pergunte o primeiro nome do usuário.
-Pergunte o sobrenome do usuário. Usando a 
-formatação de texto do comando print, imprima
-o nome completo do usuário incluindo um 
-espaço entre as palavras.'''
-
-nome = input("Primeiro nome: ")
-sobrenome = input("Sobrenome: ")
-print(f"{nome} {sobrenome}")
